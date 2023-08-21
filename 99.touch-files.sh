1b0cffdc63869f5b3eb84693c28f27209f47a3a8
hedingliang <hedingliang@eelantech.com>
2023-08-21 11:07:49 +0800

    init

---
+++ 99.touch-files.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+mkdir -p left
+mkdir -p right
+
+touch left/00.json
+touch left/01.json
+touch left/02.json
+touch left/03.json
+touch left/04.json
+touch left/05.json
+touch left/06.json
+touch left/07.json
+touch left/08.json
+touch left/09.json
+
+touch right/00.json
+touch right/01.json
+touch right/02.json
+touch right/03.json
+touch right/04.json
+touch right/05.json
+touch right/06.json
+touch right/07.json
+touch right/08.json
+touch right/09.json
+
+touch left/10.js
+touch left/11.js
+touch left/12.js
+touch left/13.js
+touch left/14.js
+touch left/15.js
+touch left/16.js
+touch left/17.js
+touch left/18.js
+touch left/19.js
+
+touch right/10.js
+touch right/11.js
+touch right/12.js
+touch right/13.js
+touch right/14.js
+touch right/15.js
+touch right/16.js
+touch right/17.js
+touch right/18.js
+touch right/19.js
