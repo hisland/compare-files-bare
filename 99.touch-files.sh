49ecbb16111632173a857ccef09d0fe533dd48b1
hedingliang <hedingliang@eelantech.com>
2024-08-14 14:55:28 +0800

    2024-08-14 14:55:28 normal commit

--- 99.touch-files.sh
+++ 99.touch-files.sh
@@ -3,46 +3,46 @@
 mkdir -p left
 mkdir -p right
 
-touch left/00.json
-touch left/01.json
-touch left/02.json
-touch left/03.json
-touch left/04.json
-touch left/05.json
-touch left/06.json
-touch left/07.json
-touch left/08.json
-touch left/09.json
+echo '中文' > left/00.json
+echo '中文' > left/01.json
+echo '中文' > left/02.json
+echo '中文' > left/03.json
+echo '中文' > left/04.json
+echo '中文' > left/05.json
+echo '中文' > left/06.json
+echo '中文' > left/07.json
+echo '中文' > left/08.json
+echo '中文' > left/09.json
 
-touch right/00.json
-touch right/01.json
-touch right/02.json
-touch right/03.json
-touch right/04.json
-touch right/05.json
-touch right/06.json
-touch right/07.json
-touch right/08.json
-touch right/09.json
+echo '中文' > right/00.json
+echo '中文' > right/01.json
+echo '中文' > right/02.json
+echo '中文' > right/03.json
+echo '中文' > right/04.json
+echo '中文' > right/05.json
+echo '中文' > right/06.json
+echo '中文' > right/07.json
+echo '中文' > right/08.json
+echo '中文' > right/09.json
 
-touch left/10.js
-touch left/11.js
-touch left/12.js
-touch left/13.js
-touch left/14.js
-touch left/15.js
-touch left/16.js
-touch left/17.js
-touch left/18.js
-touch left/19.js
+echo '中文' > left/10.js
+echo '中文' > left/11.js
+echo '中文' > left/12.js
+echo '中文' > left/13.js
+echo '中文' > left/14.js
+echo '中文' > left/15.js
+echo '中文' > left/16.js
+echo '中文' > left/17.js
+echo '中文' > left/18.js
+echo '中文' > left/19.js
 
-touch right/10.js
-touch right/11.js
-touch right/12.js
-touch right/13.js
-touch right/14.js
-touch right/15.js
-touch right/16.js
-touch right/17.js
-touch right/18.js
-touch right/19.js
+echo '中文' > right/10.js
+echo '中文' > right/11.js
+echo '中文' > right/12.js
+echo '中文' > right/13.js
+echo '中文' > right/14.js
+echo '中文' > right/15.js
+echo '中文' > right/16.js
+echo '中文' > right/17.js
+echo '中文' > right/18.js
+echo '中文' > right/19.js
